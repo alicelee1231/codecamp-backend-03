5f53e218da8a5a5de0c278f0241a713971eb17be
alicelee1231 <saemmilee1231@gmail.com>
2022-05-16 17:33:24 +0900

    UI

---
+++ class/06-02-rest-api-with-sms/backend/swagger/config.js
@@ -0,0 +1,11 @@
+export const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "나만의 미니프로젝트 API 명세서",
+      version: "0.0.1",
+    },
+  },
+  apis: ["./swagger/*.swagger.js"], // files containing annotations as above
+};
+//*파일이름 : 특정문자가 들어간 모든 파일
